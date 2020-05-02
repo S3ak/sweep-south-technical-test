0815077189aa8ee650cea43e591873b9faa3a92a
S3ak <nsineke@gmail.com>
2020-05-02 17:40:12 +0200

    Initi card component

---
+++ src/components/presentational/Card/Card.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Wrapper } from "./styled";
+
+export default function Card() {
+  return <Wrapper></Wrapper>;
+}
---
+++ src/components/presentational/Card/Card.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Card from "./Card";
+
+export default {
+  component: Card,
+  title: "Card",
+};
+
+export const standard = () => <Card />;
+export const skeleton = () => <Card />;
