2f9089bc576f6e8c57a2eee178af853b346d2e14
S3ak <nsineke@gmail.com>
2020-05-02 19:45:42 +0200

    Add params to card story

--- src/components/presentational/Card/Card.stories.js
+++ src/components/presentational/Card/Card.stories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { withKnobs, text } from "@storybook/addon-knobs";
 import Card from "./Card";
 
 import { mockCard } from "../../../utils/mocks/card";
@@ -6,7 +7,17 @@ import { mockCard } from "../../../utils/mocks/card";
 export default {
   component: Card,
   title: "Card",
+  decorators: [withKnobs],
 };
 
-export const standard = () => <Card {...mockCard} />;
+const { title, description, imageUrl } = mockCard;
+
+export const withContent = () => (
+  <Card
+    title={text("title", title)}
+    description={text("description", description)}
+    imageUrl={text("image url", imageUrl)}
+  />
+);
+
 export const skeleton = () => <Card />;
