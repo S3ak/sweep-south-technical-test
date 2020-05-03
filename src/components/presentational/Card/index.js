ae4c5f8c95f2fe43896bb1a0c0fc1f3b99e42ab8
S3ak <nsineke@gmail.com>
2020-05-03 18:48:36 +0200

    Restore card comp

--- src/components/presentational/Card/index.js
+++ src/components/presentational/Card/index.js
@@ -1,7 +1,31 @@
 import React from "react";
 
+import {
+  Wrapper,
+  MediaSection,
+  ContentSection,
+  Avatar,
+  Title,
+  Description,
+} from "./styled";
+
 export const Card = ({ title, children, imageUrl, description }) => {
-  return <div>{title}</div>;
+  return (
+    <Wrapper>
+      <MediaSection>
+        <Avatar>
+          <img src={imageUrl} />
+        </Avatar>
+      </MediaSection>
+
+      <ContentSection>
+        <Title>{title}</Title>
+        <Description>
+          {description} {children}
+        </Description>
+      </ContentSection>
+    </Wrapper>
+  );
 };
 
 export default Card;
