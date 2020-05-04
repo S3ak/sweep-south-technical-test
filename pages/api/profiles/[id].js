a752852b48150f09642c21da695707fccaf583bc
S3ak <nsineke@gmail.com>
2020-05-04 09:45:52 +0200

    Add internal API

---
+++ pages/api/profiles/[id].js
@@ -0,0 +1,23 @@
+export default async (req, res) => {
+  const {
+    query: { id },
+  } = req;
+
+  const seed = "monde";
+  const resultsLimit = 10;
+
+  const endPoint = `https://randomuser.me/api/?seed=${seed}&results=${resultsLimit}`;
+
+  let people = [];
+
+  try {
+    const resp = await fetcher(endPoint);
+    people = resp.results;
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+
+  const person = people.find((p) => p.login.uuid === id);
+
+  res.status(200).json(person);
+};
