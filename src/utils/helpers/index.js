e937479f3ed22efbac24b192e8050d03f5d5635f
S3ak <nsineke@gmail.com>
2020-05-04 16:26:36 +0200

    Add fetcher helper

--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -1,3 +1,16 @@
+import fetch from "isomorphic-unfetch";
+
 export const getUUID = () => {
   return "not-actually-unique";
+};
+
+export const fetcher = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  if (res.status !== 200) {
+    throw new Error(data.message);
+  }
+
+  return data;
 };
